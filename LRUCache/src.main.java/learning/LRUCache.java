daceb1b51a62467e5890480e6c3897497f2cc69d
kavandesai <kavan.desai@gmail.com>
2017-12-15 09:12:06 +0530

    Added LRU Cache

---
+++ LRUCache/src.main.java/learning/LRUCache.java
@@ -0,0 +1,25 @@
+package learning;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class LRUCache {
+
+	private Map<String,String> cacheMap = new ConcurrentHashMap<String,String>();
+	private ConcurrentLinkedDeque<String> lruQueue = new ConcurrentLinkedDeque<>();
+	
+	public void addElement(String e) {
+		
+	}
+	
+	public void removeElement(String e) {
+		
+	}
+	
+	public String getElement(String key) {
+		return "";
+	}
+	
+	
+}
