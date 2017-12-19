a16cc9c0145dc8a9799c5c6ac7d391223c868836
kavandesai <kavan.desai@gmail.com>
2017-12-19 20:52:43 +0530

    Check if string is anagram or not

--- Anagrams/src/learning/Anagrams.java
+++ Anagrams/src/learning/Anagrams.java
@@ -2,4 +2,27 @@ package learning;
 
 public class Anagrams {
 
+	public boolean isAnagram(String firstString, String secondString) {
+		int[] a = new int [256];
+		if (firstString.length() != secondString.length()) {
+			return false;
+		}
+		for (int i=0,j = 0;i<firstString.length();i++,j++) {
+			a[(int)firstString.charAt(i)] = ++a[(int)firstString.charAt(i)];
+			a[(int)secondString.charAt(j)] = --a[(int)secondString.charAt(j)];
+		}
+		
+		for (int i=0;i<a.length;i++) {
+			if (a[i] !=0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main (String [] args) {
+		Anagrams a = new Anagrams();
+		boolean isAnagram = a.isAnagram("aaxx", "xxaa");
+		System.out.println("Output "+isAnagram);
+	}
 }
